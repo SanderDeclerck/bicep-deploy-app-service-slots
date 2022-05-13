345ca70e1b434cd23bf1c0a021d57af9d5a3b51d
Sander Declerck <sander@sdsoftware.be>
2022-05-13 18:32:43 +0000

    Create a sample app

---
+++ sample-webapp/src/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/ping", () => Results.Json(new { result = "pong" })).WithName("Ping");
+
+app.Run();
